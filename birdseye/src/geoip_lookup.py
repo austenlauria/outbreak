4da79dbdb81332573b88a844c418ab143fb0f5e7
austenlauria <alauria@mail.csuchico.edu>
2014-12-15 10:56:58 -0800

    Edited geoip to reflect using local server

--- birdseye/src/geoip_lookup.py
+++ birdseye/src/geoip_lookup.py
@@ -2,40 +2,28 @@
 file: geoip_lookup.py
 desc: TODO
 """
-import geoip2.webservice
+
+import requests
+import json
 
 
 def geoip_lookup(ip_list):
     geoip_list = []
-    user_id = 96141
-    license = 'dpnkKYI2nCCv'
-
-    client = geoip2.webservice.Client(user_id, license)
 
     if ip_list is None or not ip_list:
         return geoip_list
 
     for ip in ip_list:
-        try:
-            response = client.city(ip["ip"])
-            geoip = dict(ip)
-            geoip["lon"] = response.location.longitude
-            geoip["lat"] = response.location.latitude
-            geoip["country"] = response.country.name
-            geoip["city"] = response.city.name
-            geoip_list.append(geoip)
-        except ValueError:
-            print "caught invalid"
-
-        # r = requests.get("http://freegeoip.net/json/" + ip["ip"])
-        # if r.status_code == 404:
-        #     continue
-        # json_object = json.loads(r.text)
-        # geoip = dict(ip)
-        # geoip["lon"] = json_object["longitude"]
-        # geoip["lat"] = json_object["latitude"]
-        # geoip["country"] = json_object["country_name"]
-        # geoip["region"] = json_object["region_name"]
-        # geoip["city"] = json_object["city"]
-        # geoip_list.append(geoip)
+        # This may be different depending on your local geoip server port
+        r = requests.get("http://localhost:8080/json/" + ip["ip"])
+        if r.status_code == 404:
+            continue
+        json_object = json.loads(r.text)
+        geoip = dict(ip)
+        geoip["lon"] = json_object["longitude"]
+        geoip["lat"] = json_object["latitude"]
+        geoip["country"] = json_object["country_name"]
+        geoip["region"] = json_object["region_name"]
+        geoip["city"] = json_object["city"]
+        geoip_list.append(geoip)
     return geoip_list
--- birdseye/tests/test_geoip_lookup.py
+++ birdseye/tests/test_geoip_lookup.py
@@ -3,7 +3,7 @@ file: test_geoip_lookup.py
 desc: TODO
 """
 
-import unittest, json
+import unittest, mock, json
 from birdseye.src import core
 
 
@@ -15,14 +15,15 @@ class TestGeoIP(unittest.TestCase):
         ]
         self.somevalid_list = [
             { "ip": "123ipyoup" },
-            { "ip": "8.8.8.8" }
+            { "ip": "google.com" }
         ]
         self.valid_list = [
-            { "ip": "8.8.8.8" },
-            { "ip": "198.41.208.143" }
+            { "ip": "google.com" },
+            { "ip": "reddit.com" }
         ]
-        self.base_url = "http://freegeoip.net/json/"
-        self.empty_geoip = json.dumps({
+        # This url may be different depending on your port for geoip server
+        self.base_url = "http://localhost:8080/json" 
+            self.empty_geoip = json.dumps({
             "longitude": "",
             "latitude": "",
             "country_name": "",
@@ -38,21 +39,41 @@ class TestGeoIP(unittest.TestCase):
         ret = core.geoip.geoip_lookup([])
         self.assertTrue(ret == [])
 
-    def test_invalid_ip_list(self):
+    @mock.patch("requests.get")
+    def test_invalid_ip_list(self, patched_get):
+        r = mock.Mock(status_code=404)
 
+        patched_get.side_effect = [r, r]
         ret = core.geoip.geoip_lookup(self.invalid_list)
 
+        self.assertTrue(patched_get.call_count == 2)
         self.assertTrue(ret == [])
 
-    def test_somevalid_ip_list(self):
+    @mock.patch("requests.get")
+    def test_somevalid_ip_list(self, patched_get):
+        bad_r = mock.Mock(status_code=404)
+        good_r = mock.Mock(status_code=200, text=self.empty_geoip)
+
+        patched_get.side_effect = [bad_r, good_r]
         ret = core.geoip.geoip_lookup(self.somevalid_list)
 
+        self.assertTrue(patched_get.call_count == 2)
         self.assertTrue(len(ret) == 1)
 
-    def test_valid_ip_list(self):
+    @mock.patch("requests.get")
+    def test_valid_ip_list(self, patched_get):
+        good_r = mock.Mock(status_code=200, text=self.empty_geoip)
 
+        patched_get.side_effect = [good_r, good_r]
         ret = core.geoip.geoip_lookup(self.valid_list)
 
+        expected = [
+            mock.call(self.base_url + self.valid_list[0]["ip"]),
+            mock.call(self.base_url + self.valid_list[1]["ip"])
+        ]
+
+        self.assertTrue(patched_get.call_args_list == expected)
+        self.assertTrue(patched_get.call_count == 2)
         self.assertTrue(len(ret) == 2)
 
 
